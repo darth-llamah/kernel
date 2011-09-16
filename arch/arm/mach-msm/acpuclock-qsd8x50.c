dc9695804ae753fae4746cacebe49229676295f7
darth-llamah <gen_scheisskopf@o2.pl>
2011-09-16 13:41:02 +0200

    fix in cpufreq table, Makefile modded for my environment

--- arch/arm/mach-msm/acpuclock-qsd8x50.c
+++ arch/arm/mach-msm/acpuclock-qsd8x50.c
@@ -74,7 +74,7 @@ struct clkctl_acpu_speed {
 struct clkctl_acpu_speed acpu_freq_tbl[] = {
         {  19200, CCTL(CLK_TCXO, 1),            SRC_RAW, 0, 0, 975, 14000 },
         { 128000, CCTL(CLK_TCXO, 1),            SRC_AXI, 0, 0, 975, 14000 },
-        { 245760, CCTL(CLK_MODEM_PLL, 1),       SRC_RAW, 0, 0, 1000, 29000 },
+        { 245000, CCTL(CLK_MODEM_PLL, 1),       SRC_RAW, 0, 0, 1000, 29000 },
         /* Work arround for acpu resume hung, GPLL is turn off by arm9 */
         /*{ 256000, CCTL(CLK_GLOBAL_PLL, 3),      SRC_RAW, 0, 0, 1000, 29000 },*/
         { 384000, CCTL(CLK_TCXO, 1),            SRC_SCPLL, 0x0A, 0, 1025, 58000 },
